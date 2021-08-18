ae042be2811f5d9e12b4fc9963e18210a3693681
Karthik <karthikeyansriramv@gmail.com>
2021-08-19 04:32:25 +0530

    update 6

--- js/app.js
+++ js/app.js
@@ -1,15 +1,60 @@
 $(document).ready(function(){
 
     function find() {
-        
-        const firstname = $('#firstname').val();
-        const middlename = $('#middlename').val();
-    
-        $('#dispfirstname').html(firstname);
-        $('#dispDiv1').show();
 
-        $('#dispmiddlename').html(middlename);
-        $('#dispDiv2').show();
+       
+        
+        const firstname = $('#fname').val();
+        const middlename = $('#mname').val();
+        const lastname = $('#lname').val();
+        const address = $('#address').val();
+        const country = $('#slect1').val();
+        const state = $('slect2').val();
+        const zipcode = $('#zipcode').val();
+        const email = $('#email').val();
+        const phno = $('#phno').val();
+        const height = $('#height').val();
+        const weight = $('#weight').val();
+
+        function isEmail($email) {
+            var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+            return regex.test($email);
+          }
+
+
+        if(firstname != "" && middlename != "" && lastname != "" && address !="" && country!="" && state != "" && zipcode != "" && email != "" && phno != ""){
+    
+          $('#dispfirstname').html(firstname);
+          $('#dispmiddlename').html(middlename);
+          $('#displastname').html(lastname);
+          $('#dispaddress').html(address+', '+ country+', '+ state+', '+ zipcode);
+          $('#dispemail').html(email);
+          $('#dispphno').html(phno);
+          $('#disph').html(height);
+          $('#dispw').html(weight);
+          
+  
+  
+          $('#dispDiv1').show();
+          $('#dispDiv2').show();
+          $('#dispDiv3').show();
+          $('#dispDiv4').show();
+          $('#dispDiv5').show();
+          $('#dispDiv6').show();
+          $('#dispDiv7').show();
+          $('#dispDiv8').show();
+        
+    }else if(!isEmail(email) ){
+     
+      alert("Enter a Valid Email Id");
+        
+    }else{
+
+      alert("Fill All Details");
+
+    }
+        
+
     }
 
     $("#submit").click(function(){
@@ -22,13 +67,13 @@ $(document).ready(function(){
         allowClear: true
     });
 
-    function populate()  {
+    function populate(s1,s2)  {
 
 
         var s1 = $('#slect1');
         var s2 = $('#slect2');
 
-        if (s1.val == "['United States','US']") {
+        if (s1.val == 'United States of America') {
             var optionArray = [
               "|",
               "camaro|Camaro",
