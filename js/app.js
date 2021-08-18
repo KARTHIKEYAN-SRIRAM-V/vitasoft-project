723cddfae88db2e1eea3cdf3c90155837c5207b5
Karthik <karthikeyansriramv@gmail.com>
2021-08-18 20:01:14 +0530

    update 4

--- js/app.js
+++ js/app.js
@@ -11,6 +11,31 @@ $(document).ready(function(){
     find()
 
     });
+
+    function populate()  {
+
+
+        var s1 = $('#select1');
+        var s2 = $('#select2');
+
+        if (s1.val == "['United States','US']") {
+            var optionArray = [
+              "|",
+              "camaro|Camaro",
+              "corvette|Corvette",
+              "impala|Impala",
+            ];
+          }
+
+          for (var option in optionArray) {
+            var pair = optionArray[option].split("|");
+            var newOption = document.createElement("option");
+            newOption.value = pair[0];
+            newOption.innerHTML = pair[1];
+            s2.options.add(newOption);
+          }
+        }
+
   });
 
   
